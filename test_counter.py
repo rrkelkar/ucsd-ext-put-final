dc55570713ef139e586940c681cafb64de985703
Rama Kelkar <61393665+rrkelkar@users.noreply.github.com>
2020-05-30 13:38:31 -0700

    Update test_counter.py

--- test_counter.py
+++ test_counter.py
@@ -6,3 +6,11 @@ def test_counter_inc():
         
 def test_counter_dec():
          assert counter.dec(5) == 4
+        
+def test_counter_inc_by_2():
+         assert counter.dec(4) == 6
+
+def test_counter_dec_by_2():
+         assert counter.dec(6) == 4
+
+    
