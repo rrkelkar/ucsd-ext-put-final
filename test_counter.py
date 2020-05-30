906d7ed3986a37bc72143b13aafb87a38b0e6a6f
Rama Kelkar <61393665+rrkelkar@users.noreply.github.com>
2020-05-30 13:41:58 -0700

    Update test_counter.py

--- test_counter.py
+++ test_counter.py
@@ -8,9 +8,9 @@ def test_counter_dec():
          assert counter.dec(5) == 4
         
 def test_counter_inc_by_2():
-         assert counter.dec(4) == 6
+         assert counter.inc_by_2(4) == 6
 
 def test_counter_dec_by_2():
-         assert counter.dec(6) == 4
+         assert counter.dec_by_2(6) == 4
 
     
